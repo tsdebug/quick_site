8500e0081755837663fc77b3bb293b6fbed00af1
Tsdebug <taworkspace01@gmail.com>
2026-01-20 21:31:54 +0530

    Updated assests

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -175,14 +175,14 @@ export default function HeroSection() {
                                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                                     <Image
                                         className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                                        src="/mail2.png"
+                                        src="/mail2.webp"
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
                                     />
                                     <Image
                                         className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                                        src="/mail2-light.png"
+                                        src="/mail2-light.webp"
                                         alt="app screen"
                                         width="2700"
                                         height="1440"
