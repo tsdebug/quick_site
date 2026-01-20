f7bf7d0dc2e9d80ef00ef25bb153eb4c7096afb7
Tsdebug <taworkspace01@gmail.com>
2026-01-20 21:08:30 +0530

    Added a Tailark component for stats section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Features from "@/components/features-3";
 import HeroSection from "@/components/hero-section";
+import IntegrationsSection from "@/components/integrations-3";
+import StatsSection from "@/components/stats-3";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +9,8 @@ export default function Home() {
     <div>
       <HeroSection />
       <Features />
+      <IntegrationsSection />
+      <StatsSection />
     </div>
   );
 }
