fa1b8db9220336db2978972ad84ce58bc9264ebf
Tsdebug <taworkspace01@gmail.com>
2026-01-20 21:06:51 +0530

    Added a Tailark component for integration section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Features from "@/components/features-3";
 import HeroSection from "@/components/hero-section";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
+      <Features />
     </div>
   );
 }
