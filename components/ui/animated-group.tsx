f86e810c1b1e2bff1f133ab8ed5be371fd85207a
Tsdebug <taworkspace01@gmail.com>
2026-01-20 23:14:09 +0530

    Changed keyof 'JSX.IntrinsicElements' to 'any' to solve build issue

--- components/ui/animated-group.tsx
+++ components/ui/animated-group.tsx
@@ -116,11 +116,11 @@ function AnimatedGroup({
   const itemVariants = variants?.item || selectedVariants.item;
 
   const MotionComponent = React.useMemo(
-    () => motion.create(as as keyof JSX.IntrinsicElements),
+    () => motion.create(as as any),
     [as]
   );
   const MotionChild = React.useMemo(
-    () => motion.create(asChild as keyof JSX.IntrinsicElements),
+    () => motion.create(asChild as any),
     [asChild]
   );
 
